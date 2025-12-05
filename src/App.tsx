0615d6f5be8057fc4eca6f57771528547ee91982
Zhaslanuly Sabyrzhan <sabyrkaw@gmail.com>
2025-12-06 00:18:40 +0500

    feat: load tasks from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,19 @@
-import { useState } from 'react'
-
-const tasks = [
-  {
-    id: 1,
-    title: 'Купить продукты на неделю',
-    isDone: false,
-    addedAt: '1 сентября',
-    priority: 2,
-  },
-  {
-    id: 2,
-    title: 'Полить цветы',
-    isDone: true,
-    addedAt: '2 сентября',
-    priority: 0,
-  },
-  {
-    id: 3,
-    title: 'Сходить на тренировку',
-    isDone: false,
-    addedAt: '3 сентября',
-    priority: 1,
-  },
-  {
-    id: 4,
-    title: 'Срочно отправить рабочий отчет',
-    isDone: false,
-    addedAt: '4 сентября',
-    priority: 4,
-  },
-  {
-    id: 5,
-    title: 'Заплатить за коммунальные услуги',
-    isDone: false,
-    addedAt: '3 сентября',
-    priority: 3,
-  },
-]
+import { useEffect, useState } from 'react'
 
 const priorities = ['#fff', '#ffd7b5', '#ffb38a', '#ff9248', '#ff6700']
 
 function App() {
-  const [selectedTaskId, setSelectedTaskId] = useState<number | null>(null)
+  const [selectedTaskId, setSelectedTaskId] = useState(null)
+  const [tasks, setTasks] = useState(null)
+
+  useEffect(() => {
+    fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
+      headers: {
+        'api-key': import.meta.env.VITE_API_KEY,
+      },
+    }).then(res => res.json())
+      .then((task) => setTasks(task.data))
+  }, [])
 
   if (tasks === null) return <h1>Загрузка...</h1>
 
@@ -59,7 +31,7 @@ function App() {
             key={task.id}
             onClick={() => setSelectedTaskId(task.id)}
             style={{
-              backgroundColor: priorities[task.priority],
+              backgroundColor: priorities[task.attributes.priority],
               border: `2px solid ${task.id === selectedTaskId ? 'blue' : 'transparent'}`,
             }}
           >
@@ -67,22 +39,22 @@ function App() {
               <b>Заголовок: </b>
               <span
                 style={{
-                  textDecorationLine: task.isDone ? 'line-through' : 'none',
+                  textDecorationLine: task.attributes.status ? 'line-through' : 'none',
                 }}
               >
-              {task.title}
+              {task.attributes.title}
             </span>
             </p>
             <p>
               <b>Статус: </b>
               <input
                 type="checkbox"
-                defaultChecked={task.isDone}
+                defaultChecked={task.attributes.status}
               />
             </p>
             <p>
               <b>Дата создания задачи: </b>
-              <span>{task.addedAt}</span>
+              <span>{new Date(task.attributes.addedAt).toLocaleDateString()}</span>
             </p>
           </li>
         ))}
