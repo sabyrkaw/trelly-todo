e30ce43f2559203093b051a5cc005765ca1b4d24
Zhaslanuly Sabyrzhan <sabyrkaw@gmail.com>
2025-12-05 18:09:14 +0500

    feat: render algorithm based on data structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,81 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Купить продукты на неделю",
+    isDone: false,
+    addedAt: "1 сентября",
+    priority: 2,
+  },
+  {
+    id: 2,
+    title: "Полить цветы",
+    isDone: true,
+    addedAt: "2 сентября",
+    priority: 0,
+  },
+  {
+    id: 3,
+    title: "Сходить на тренировку",
+    isDone: false,
+    addedAt: "3 сентября",
+    priority: 1,
+  },
+  {
+    id: 4,
+    title: "Срочно отправить рабочий отчет",
+    isDone: false,
+    addedAt: "4 сентября",
+    priority: 4,
+  },
+  {
+    id: 5,
+    title: "Заплатить за коммунальные услуги",
+    isDone: false,
+    addedAt: "3 сентября",
+    priority: 3,
+  },
+]
+
 function App() {
-  const tasks = [
-    { id: 1, title: "Купить продукты на неделю", isDone: false },
-    { id: 2, title: "Полить цветы", isDone: true },
-    { id: 3, title: "Сходить на тренировку", isDone: false },
-  ]
+  const priorities = ['#fff', '#ffd7b5', '#ffb38a', '#ff9248', '#ff6700']
+
+  if (tasks === null) return <h1>Загрузка...</h1>
+
+  if (tasks.length === 0) return <h1>Задачи отсутствуют</h1>
 
   return (
-    <>
-      <h1>Список дел</h1>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <div>{task.title}</div>
-            <input type="checkbox" defaultChecked={task.isDone} />
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      {tasks.map((task) => (
+        <li
+          key={task.id}
+          style={{
+            backgroundColor: priorities[task.priority],
+          }}
+        >
+          <p>
+            <b>Заголовок: </b>
+            <span
+              style={{
+                textDecorationLine: `${task.isDone ? 'line-through' : 'none'}`,
+              }}
+            >
+              {task.title}
+            </span>
+          </p>
+          <p>
+            <b>Статус: </b>
+            <input
+              type="checkbox"
+              defaultChecked={task.isDone}
+            />
+          </p>
+          <p>
+            <b>Дата создания задачи: </b>
+            <span>{task.addedAt}</span>
+          </p>
+        </li>
+      ))}
+    </ul>
   )
 }
 
