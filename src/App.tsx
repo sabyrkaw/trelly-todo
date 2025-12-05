ae95db7b6c7e2d83137b9fffb7e3bc70124ad091
Zhaslanuly Sabyrzhan <sabyrkaw@gmail.com>
2025-12-05 11:30:57 +0500

    feat: scaffold Vite project & rewrite markup to React

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  const tasks = [
+    { id: 1, title: "Купить продукты на неделю", isDone: false },
+    { id: 2, title: "Полить цветы", isDone: true },
+    { id: 3, title: "Сходить на тренировку", isDone: false },
+  ]
+
+  return (
+    <>
+      <h1>Список дел</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <div>{task.title}</div>
+            <input type="checkbox" defaultChecked={task.isDone} />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
